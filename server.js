369b5c518d3db00f96cd47220db2ebf0da003272
zekraquraan <zekraquraan7@gmail.com>
2023-04-07 14:45:48 +0000

    updated

--- server.js
+++ server.js
@@ -1,50 +1,45 @@
 'use strict';
-const express = require('express')
-const cors=require('cors');
+const express = require('express');
+const movieData=require('./data.json')
 const app = express()
-app.use(cors());
-const port = 3000
-const movieData=require("./Movie Data/data.json")
-app.get('/',myMovieApp);
-app.get('/favorite',myFirstMovie);
-app.get('*',handlePageNotFoundError);
-app.get('*',handleServerError);
+
+const port = 3000;
 
 
-function myMovieApp(req, res) {
-  
-  let newConst=new Constmovie(movieData.title,movieData.poster_path,movieData.overview);
-  res.json(newConst);
+
+app.get('/',homePageHandeler);
+app.get('/favourite',favPageHandeler);
+app.get('*',handleNotFoundError);
+app.post('*',handleServerError);
+
+function homePageHandeler(req,res){
+    let result= new Movie(movieData.title,movieData.poster_path,movieData.overview);
+    res.json(result);
+
+}
+
+function favPageHandeler(req,res){
+
+    res.send('Welcome to Favorite Page');
+
+}
+
+function handleNotFoundError(req,res){
+    res.status(404).send("page not found error")
+}
+
+ function handleServerError(req, res) {
+    res.status(500).send(new Error())
+  }
+
+function Movie(title,poster_path,overview){
+    this.title=title;
+    this.poster_path=poster_path;
+    this.overview=overview
 }
 
 
-function Constmovie(title,poster_path,overview){
-this.title=title;
-this.poster_path=poster_path;
-this.overview=overview;
-}
-
-
-
-function myFirstMovie(req,res){
-    res.send("Welcome to Favorite Page");
-}
 
 
 app.listen(port, () => {
-  console.log(`Movie app listening on port ${port}`)
-})
-
-
-
-function handlePageNotFoundError() {
-    res.status(404).send("NOT FOUND");
-  }
-
-
-
-
-  function handleServerError() {
-    res.status(500).send ("Sorry, something went wrong");
-    
-  }
+  console.log(`Example app listening on port ${port}`)})
